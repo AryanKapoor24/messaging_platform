ff1ad9c561d31d799849741cd330112eb98da7d8
ARYAN KAPOOR <aryanalokkapoor@gmail.com>
2024-06-07 18:23:40 +0530

    k

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import cors from 'cors'; // Import CORS package
 import { Telegraf } from 'telegraf';
 import dotenv from 'dotenv';
+import { message } from 'telegraf/filters';
 
 dotenv.config();
 
@@ -11,7 +12,7 @@ const bot = new Telegraf('7094725314:AAHF_GF-hisXhDbrpYEre9DW0gOYXp9ZJA4');
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
-app.use(cors()); // Enable CORS for all routes
+app.use(cors()); 
 
 // Routes and bot integration as previously defined...
 
@@ -22,20 +23,21 @@ bot.on('text', (ctx) => {
         sender: 'Telegram User',
         message: ctx.message.text
     };
+    //ctx.reply("hiiii");
 });
 
 bot.launch();
 
 app.post('/api/send', (req, res) => {
     const { message } = req.body;
-    // Replace 'YOUR_CHAT_ID' with the actual chat ID
+    
     bot.telegram.sendMessage('5949702687', message); 
     res.sendStatus(200);
 });
 
 app.get('/api/messages', (req, res) => {
     res.json(lastMessage);
-    lastMessage = null; // Resetting lastMessage after it's been sent
+    lastMessage = null; 
 });
 
 const PORT = process.env.PORT || 5000;
